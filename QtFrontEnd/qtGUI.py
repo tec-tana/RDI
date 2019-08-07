221e84356c63be5bf5a914e571bae76d8faaba9f
Tech Tana <tana.tech.95@gmail.com>
2019-08-07 01:33:06 -0400

    Add plot to qt GUI & zoom-in capability

--- QtFrontEnd/qtGUI.py
+++ QtFrontEnd/qtGUI.py
@@ -5,13 +5,17 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
         QVBoxLayout, QWidget, QStyleFactory)
-import pyqtgraph
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
+
 
 
 class WidgetGallery(QDialog):
     def __init__(self, parent=None):
         super(WidgetGallery, self).__init__(parent)
-        self.setMinimumSize(1500, 1000)
+        self.setMinimumSize(2000, 1500)
 
         # Create widgets
         self.create_topLayout()
@@ -92,23 +96,76 @@ class WidgetGallery(QDialog):
         self.plot_box = QGroupBox("Plot: " + self.styleComboBox.currentText())
         self.styleComboBox.activated.connect(self.update_group_title)
 
+        # embed plot on the widget
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # Just some button connected to `plot` method
+        self.button = QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+
         # Create one-line text widget
-        lineEdit = QLineEdit('s3cRe7')
-        lineEdit.setEchoMode(QLineEdit.Normal)  # display characters as entered, no masking
+        self.lineEdit = QLineEdit('Put comments here...')
+        self.lineEdit.setEchoMode(QLineEdit.Normal)  # display characters as entered, no masking
 
         # Create slider widget
-        slider = QSlider(Qt.Horizontal, self.plot_box)
-        slider.setValue(40)
+        self.slider = QSlider(Qt.Horizontal, self.plot_box)
+        self.slider.setMaximum(150)
+        self.slider.setMinimum(1)
+        self.slider.setValue(100)
+        self.slider.valueChanged.connect(self.setPlotZoom)
 
         # Create Grid Layout object
         layout = QGridLayout()
-        layout.addWidget(lineEdit, 4, 0, 1, 2)
-        layout.addWidget(slider, 5, 0, 1, 2)
+        layout.addWidget(self.toolbar, 0, 0, 1, 2)
+        layout.addWidget(self.canvas, 1, 0, 2, 2)
+        layout.addWidget(self.button, 3, 0, 1, 2)
+        layout.addWidget(self.lineEdit, 4, 0, 1, 2)
+        layout.addWidget(self.slider, 5, 0, 1, 2)
         layout.setRowStretch(0, 1)
         self.plot_box.setLayout(layout)
 
     def update_group_title(self):
         self.plot_box.setTitle("Plot: " + self.styleComboBox.currentText())
+
+    def plot(self):
+        """
+        plot some random stuff
+
+        :return:
+        """
+        # random data
+        self.data = [random.random() for i in range(100)]
+
+        # instead of ax.hold(False)
+        self.figure.clear()
+
+        # create an axis
+        self.ax = self.figure.add_subplot(111)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        self.ax.plot(self.data, '*-')
+        self.ax.set_xlim(xmin=0, xmax=len(self.data) * self.slider.value() / 100)
+
+        # refresh canvas
+        self.canvas.draw()
+
+    def setPlotZoom(self):
+        try:
+            self.ax.set_xlim(xmin=0, xmax=len(self.data)*self.slider.value()/100)
+            self.canvas.draw()
+        except AttributeError:  # if the slider is moved before plot is generated
+            pass
 
 
 if __name__ == '__main__':
